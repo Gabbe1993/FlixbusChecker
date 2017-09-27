763fbd3830c64330f3af45d6ea964019b06e29c6
Gabriel Vilen <gabriel.vilen@gmail.com>
2017-09-27 15:33:02 +0200

    email notification with price drop

--- src/script.js
+++ src/script.js
@@ -4,35 +4,71 @@
 
 var http = require('http');
 var request = require('request');
-var html2json = require('html2json').html2json;
+var nodemailer = require('nodemailer');
 
+// url is result from search page
 var url = 'https://shop.flixbus.com/search?route=Berlin-Delft&rideDate=01.10.2017&adults=1&children=0&bikes=0&_locale=en&departureCity=88&departureStation=&arrivalCity=3378&arrivalStation=&currency=EUR';
-var res;
+var notifyIfLess = 60;
 
-function wrapper(){
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'xxx', //  your email
+        pass: 'xxx'   // your pass
+    }
+});
+
+var mailOptions = {
+    from: 'FlixbusChecker',
+    to: 'xxx',  // retriever of the email
+    subject: 'Price drop for Flixbus! Now: ',
+    text: 'Best regards from FlixbusChecker :)'
+};
+
+function sendMailWrap() {
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    }
+)}
+
+function requestWrap(){
     request(url, function (error, response, body) {
-
-        if(error)
+        if(error) {
             console.log('error:', error); // Print the error if one occurred
-        // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+        }
+        console.log('statusCode:', response && response.statusCode);
 
         var toMatch = "class=\"num\"";
         var index = body.indexOf(toMatch);
+        var price = parseInt(body.substr(index + toMatch.length + 1, 5));
 
-        this.res = parseInt(body.substr(index + toMatch.length + 1, 5));
+        console.log("Price: " + price);
 
-        console.log(this.res);
+        if(price < notifyIfLess) {
+            mailOptions.subject += price + ' €';
+            notifyIfLess = price;
+            console.log('Less than set to = ' + notifyIfLess);
+
+            sendMailWrap();
+        }
     });
 }
+setInterval(requestWrap, 60000); // checks price every given ms
 
-http.createServer(function (req, response) {
+/*http.createServer(function (req, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
+    console.log('Create server');
 
-    setInterval(wrapper, 60000); // checks every 60 sek
 
-    response.write("" + this.res);
+    response.write('Running FlixbusChecker');
+    response.end('');
 
-}).listen(8888);
+    console.log('Server running at http://127.0.0.1:8889/');
 
-console.log('Server running at http://127.0.0.1:8888/');
+ }).listen(8889);
+*/
 
